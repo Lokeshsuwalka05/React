d033ff7334a28ca8831951931116b33a34f8dcbe
unknown <lokeshsuwalka2002@gmail.com>
2025-07-19 20:46:13 +0530

    Added some comments in App.js file

--- Assignment1/App.js
+++ Assignment1/App.js
@@ -1,5 +1,7 @@
   // const heading=React.createElement("h1",{id:"heading"},"hello world form React");
   const root=ReactDOM.createRoot(document.getElementById("root"));
+  // It's the job of ReactDOM to create root i.e, heading in this case, bcz root is a dom element which is rendered to be on browser. This root is the place where all React code will run.
+
       // <div id="parent">
       //      <div id="child1">
       //         <h1></h1>
@@ -23,3 +25,4 @@
 
   );
   root.render(parent);
+  // In React, Render is the technique that can redirect a page with the help of function render(). Most importantly, render a function we can use to define the HTML code within the HTML element. It helps to display certain views in the UI using certain logic defined in the render function and returns the output.
